45678407307eafa328a0017e0ca01d5c47e1715a
Shivam Maheshwari <theshivammaheshwari@gmail.com>
2025-05-23 01:23:37 +0530

    V11

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/rform">
+      <BrowserRouter basename="/requisition">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
